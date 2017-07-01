a908d4cd000f1dd9b79eaecaa22159dfc3a40bcb
Nick <nfuller52@gmail.com>
2017-06-30 19:30:51 -1000

    Remove commented out navigation

--- site/web/app/themes/legion-del-diablo/partials/navigation.php
+++ site/web/app/themes/legion-del-diablo/partials/navigation.php
@@ -6,23 +6,14 @@
     <a class="navbar-brand" href="<?php echo home_url(); ?>">
         legion dèl <span class="primary">diablo</span>
     </a>
-    <!-- <div class="collapse navbar-collapse" id="main-nav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item"><a class="nav-link" href="#news">News</a></li>
-        <li class="nav-item"><a class="nav-link" href="#progress">Progress</a></li>
-        <li class="nav-item"><a class="nav-link" href="#recruitment">Recruitment</a></li>
-        <li class="nav-item"><a class="nav-link" href="#forum">Forum</a></li>
-        <li class="nav-item"><a class="nav-link" href="#roster">Roster</a></li> -->
-        <?php wp_nav_menu( array(
-            'theme_location'  => 'primary',
-            'container'       => 'div',
-            'container_class' => 'collapse navbar-collapse',
-            'container_id'    => 'main-nav',
-            'menu_class'      => 'navbar-nav ml-auto',
-            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-            'walker'          => new WP_Bootstrap_Navwalker()
-        ) ); ?>
-      <!-- </ul>
-    </div> -->
+    <?php wp_nav_menu( array(
+        'theme_location'  => 'primary',
+        'container'       => 'div',
+        'container_class' => 'collapse navbar-collapse',
+        'container_id'    => 'main-nav',
+        'menu_class'      => 'navbar-nav ml-auto',
+        'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'          => new WP_Bootstrap_Navwalker()
+    ) ); ?>
   </div>
 </nav>
